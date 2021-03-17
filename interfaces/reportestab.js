ecf2a15225bfdaa15bb8de56da94c2f035ffca84
JoseLuisGAparicio <1717110268@utectulancingo.edu.mx>
2021-03-17 17:18:33 -0600

    correccion de errores en el boton de pdf

--- interfaces/reportestab.js
+++ interfaces/reportestab.js
@@ -20,12 +20,6 @@ $(document).ready(function() {
         responsive: "true",
         dom: 'Bfrtilp',
         buttons: [{
-                extend: 'excelHtml5',
-                text: '<i class="fas fa-file-excel"></i> ',
-                titleAttr: 'Exportar a Excel',
-                className: 'btn btn-success'
-            },
-            {
                 extend: 'pdfHtml5',
                 text: '<i class="fas fa-file-pdf"></i> ',
                 titleAttr: 'Exportar a PDF',
