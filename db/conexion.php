c05953ab21e181b855b43e050bbc11b823063943
¨danielarubialesmarquez¨ <1717110247@utectulancingo.edu.mx>
2021-04-01 12:53:45 -0600

    bajando cambios

--- db/conexion.php
+++ db/conexion.php
@@ -1,4 +1,9 @@
 <?php
+///$servername = "sql305.epizy.com";
+//$database = "epiz_28273132_db_customer_assistant";
+//$username = "epiz_28273132";
+//$password = "ipyTod677Jd26gB";
+
 $servername = "localhost";
 $database = "db_customer_assistant";
 $username = "root";
